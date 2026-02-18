57c9ba6a997dc2c2f0032ed9f533d9c7a384bccf
diogo241 <diogomesquita241@gmail.com>
2026-02-18 15:51:07 +0000

    feat: setup tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-pt">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>{{ config('app.name') }}</title>
+
+  @vite(['resources/css/app.css'])
 </head>
 
 <body>
-  @foreach ($habits as $h)
-    <p> {{ $h }} </p>
-  @endforeach
-
+  <h1 class="text-3xl font-bold underline text-green-500">Teste</h1>
 </body>
 
 </html>
